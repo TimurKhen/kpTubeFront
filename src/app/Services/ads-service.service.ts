30ab2cc75ac194c2a76a869edc0fbe7bbad99f8f
Gvinses <timurkhen10@gmail.com>
2025-06-01 22:17:18 +0300

    clean of file

--- src/app/Services/ads-service.service.ts
+++ src/app/Services/ads-service.service.ts
@@ -8,13 +8,13 @@ export class AdsServiceService {
   constructor() { }
 
   private adsInformation = [
-    {
-      "name": 'evgeniyaADS.jpg',
-      "author": 'Пожилой Жека',
-      "text": 'Наглые подростки совсем потеряли совесть и сделали ЭТО прямо в спальне родителей. Уберите детей от экранов!!!!!\n' +
-        'Смотреть только на KPtube:  Пожилой Жека',
-      "link": 'https://kptube.netlify.app/account/1725207636522'
-    },
+    // {
+    //   "name": 'evgeniyaADS.jpg',
+    //   "author": 'Пожилой Жека',
+    //   "text": 'Наглые подростки совсем потеряли совесть и сделали ЭТО прямо в спальне родителей. Уберите детей от экранов!!!!!\n' +
+    //     'Смотреть только на KPtube:  Пожилой Жека',
+    //   "link": 'https://kptube.netlify.app/account/1725207636522'
+    // },
     {
       "name": '../KptubeLogo.svg',
       "author": 'KP229',
