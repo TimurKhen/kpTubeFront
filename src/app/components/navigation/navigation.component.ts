f4e49b40d481bee7bb89f62920faf4d9fe3e2da7
Timur Khen <timurkhen10@gmail.com>
2026-03-29 22:02:53 +0300

    textarea update

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,10 +3,11 @@ import { ProfilePreview } from '../../interfaces/profile/preview';
 import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MenuOpenerComponent } from "./menu-opener/menu-opener.component";
 import { NgClass } from '@angular/common';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'navigation',
-  imports: [MenuOpenerComponent, ReactiveFormsModule, NgClass],
+  imports: [MenuOpenerComponent, ReactiveFormsModule, NgClass, RouterLink],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss',
 })
--- src/app/pages/videos-grid/videos-grid.component.ts
+++ src/app/pages/videos-grid/videos-grid.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { VideosService } from '../../services/videos-service/videos-service.service';
 import { VideoPreview } from '../../interfaces/video/preview';
 import { ContentBoxComponent } from "../../components/content-box/content-box.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-videos-grid',
-  imports: [ContentBoxComponent],
+  imports: [ContentBoxComponent, RouterLink],
   templateUrl: './videos-grid.component.html',
   styleUrl: './videos-grid.component.scss',
 })
