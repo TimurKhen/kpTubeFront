4d2d6dbfbb9d245225c4e83f8121e061a8df0dd7
Timur Khen <timurkhen10@gmail.com>
2026-04-18 12:46:16 +0300

    path fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,7 +10,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(
       routes,
-      withHashLocation(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'top',
       }),
