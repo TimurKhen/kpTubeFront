8c6f88741b5b5da443a0893f4ebc2c3ed81a5f91
Timur Khen <timurkhen10@gmail.com>
2026-04-18 12:20:19 +0300

    Auto-generated commit

--- chunk-IHAWOZWG.js
+++
@@ -1,1 +0,0 @@
-import{o as a}from"./chunk-A6CJ3FOW.js";import"./chunk-CK7CK7OI.js";import"./chunk-IBNURS6N.js";export{a as ContentBoxComponent};
---
+++ chunk-QY5BKL2P.js
@@ -0,0 +1,1 @@
+import{p as a}from"./chunk-5FMB3QQS.js";import"./chunk-CK7CK7OI.js";import"./chunk-IBNURS6N.js";export{a as ContentBoxComponent};
